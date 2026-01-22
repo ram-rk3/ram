0b4845e5b6a3a537cf1820b90fb76c6fc3357b83
RAMKUMAR G <ramkumarff2k@gmail.com>
2026-01-22 21:29:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 df = pd.read_csv("vehicle_service_dataset.csv")
 
@@ -25,16 +24,5 @@ rf_model = RandomForestRegressor(
 rf_model.fit(X_train, y_train)
 
 y_pred = rf_model.predict(X_test)
+Predicted= y_pred
 
-print("Model Evaluation Metrics:\n")
-print("Mean Absolute Error (MAE):", mean_absolute_error(y_test, y_pred))
-print("Root Mean Squared Error (RMSE):", np.sqrt(mean_squared_error(y_test, y_pred)))
-print("R² Score:", r2_score(y_test, y_pred))
-
-
-predictions_df = pd.DataFrame({
-    'Actual': y_test,
-    'Predicted': y_pred
-})
-print("\nSample Predictions vs Actual:")
-print(predictions_df.head())
